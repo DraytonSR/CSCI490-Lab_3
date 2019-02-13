ee1bfc7d7ea8c0cf48b62f287abf8e4c4f7800a7
DraytonScott <42654988+DraytonScott@users.noreply.github.com>
2019-02-13 18:29:30 -0500

    Did the thing

--- AndroidStudioProjects/ListViewDemo/app/src/main/java/com/example/dscott/listviewdemo/MainActivity.java
+++ AndroidStudioProjects/ListViewDemo/app/src/main/java/com/example/dscott/listviewdemo/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.dscott.listviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.class_schedule);
+
+        String[] itemList = {"CSCI 360", "CSCI 362", "ENGL 212", "CSCI 332", "CSCI 340"};
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, itemList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Short click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Long click", Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+        });
     }
 }
